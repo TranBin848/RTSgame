d9f7672bd72313a9059e968558b94f165e38ea14
TranBin848 <tranbinn848@gmail.com>
2026-04-10 22:23:45 +0700

    Add input touch

--- Assets/Game/Scripts/Managers/GameManager.cs
+++ Assets/Game/Scripts/Managers/GameManager.cs
@@ -3,10 +3,27 @@ using UnityEngine;
 
 public class GameManager : SingletonManager<GameManager>
 {
-
-
-    public void Test()
+    private Vector2 m_initialTouchPosition;
+    void Update()
     {
-        Debug.Log("GameManager Test method called.");
+        Vector2 inputPosition = Input.touchCount > 0 ? Input.GetTouch(0).position : Input.mousePosition;
+
+        if (Input.GetMouseButtonDown(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            m_initialTouchPosition = inputPosition;
+        }
+
+        if (Input.GetMouseButtonUp(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended))
+        {
+            if (Vector2.Distance(m_initialTouchPosition, inputPosition) < 10f)
+            {
+                DeleteClick(inputPosition);
+            }
+        }
+    }
+
+    void DeleteClick(Vector2 inputPosition)
+    {
+        Debug.Log($"Clicked at screen position: {inputPosition}");
     }
 };
