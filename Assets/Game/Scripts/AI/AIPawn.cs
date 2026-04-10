b6a643959022fe2645dbae6f3f0baf235efe2c64
TranBin848 <tranbinn848@gmail.com>
2026-04-10 20:10:05 +0700

    Create unit class and basic move

---
+++ Assets/Game/Scripts/AI/AIPawn.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+public class AIPawn : MonoBehaviour
+{
+    [SerializeField] private float m_Speed = 1.0f;
+    private Vector3? m_Destination;
+    public Vector3 Destination
+    {
+        get => m_Destination ?? transform.position;
+    }
+
+    void Start()
+    {
+        SetDestination(new Vector3(-7.0f, -2.0f, 0));
+    }
+
+    void Update()
+    {
+        if (m_Destination.HasValue)
+        {
+            var dir = m_Destination.Value - transform.position;
+            transform.position += dir.normalized * Time.deltaTime * m_Speed;
+        }
+
+        var distanceToDestination = Vector3.Distance(transform.position, Destination);
+        if (distanceToDestination < 0.1f)
+        {
+            m_Destination = null;
+        }
+    }
+
+    public void SetDestination(Vector3 destination)
+    {
+        m_Destination = destination;
+    }
+
+
+}
