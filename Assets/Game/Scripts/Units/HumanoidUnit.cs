fe2a9a3e9e0e7a739ba01f68b414200a5af79574
TranBin848 <tranbinn848@gmail.com>
2026-04-10 21:00:49 +0700

    Add logic change run state

--- Assets/Game/Scripts/Units/HumanoidUnit.cs
+++ Assets/Game/Scripts/Units/HumanoidUnit.cs
@@ -1,4 +1,23 @@
 using UnityEngine;
 public class HumanoidUnit : Unit
 {
+    protected Vector2 m_Velocity;
+    protected Vector3 m_lastPosition;
+    public float CurrentSpeed => m_Velocity.magnitude;
+
+    void Update()
+    {
+        m_Velocity = new Vector2(
+            transform.position.x - m_lastPosition.x,
+            transform.position.y - m_lastPosition.y
+            ) / Time.deltaTime;
+
+        m_lastPosition = transform.position;
+        isMoving = m_Velocity.magnitude > 0.01f;
+
+        m_Animator.SetFloat("Speed", CurrentSpeed);
+    }
+
 }
+
+
--- Assets/Game/Scripts/Units/Unit.cs
+++ Assets/Game/Scripts/Units/Unit.cs
@@ -2,5 +2,14 @@ using UnityEngine;
 
 public abstract class Unit : MonoBehaviour
 {
+    public bool isMoving = false;
+    protected Animator m_Animator;
 
+    protected void Awake()
+    {
+        if (TryGetComponent<Animator>(out var animator))
+        {
+            m_Animator = animator;
+        }
+    }
 }
