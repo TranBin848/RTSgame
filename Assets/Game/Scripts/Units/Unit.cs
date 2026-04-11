68127d8a1f3b9aa458122b7ac910d5c447509025
TranBin848 <tranbinn848@gmail.com>
2026-04-11 12:34:20 +0700

    Change pipeline to URP and add shader

--- Assets/Game/Scripts/Units/Unit.cs
+++ Assets/Game/Scripts/Units/Unit.cs
@@ -5,7 +5,7 @@ public abstract class Unit : MonoBehaviour
     public bool isMoving = false;
     protected Animator m_Animator;
     protected AIPawn m_AIPawn;
-
+    protected SpriteRenderer m_SpriteRenderer;
     protected void Awake()
     {
         if (TryGetComponent<Animator>(out var animator))
@@ -16,9 +16,16 @@ public abstract class Unit : MonoBehaviour
         {
             m_AIPawn = aiPawn;
         }
+        if (TryGetComponent<SpriteRenderer>(out var spriteRenderer))
+        {
+            m_SpriteRenderer = spriteRenderer;
+        }
     }
     public void MoveTo(Vector3 destination)
     {
+        var direction = (destination - transform.position).normalized;
+        m_SpriteRenderer.flipX = direction.x < 0;
+
         if (m_AIPawn != null)
         {
             m_AIPawn.SetDestination(destination);
