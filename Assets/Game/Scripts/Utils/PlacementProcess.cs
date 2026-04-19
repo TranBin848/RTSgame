bce9fdd6f300b550e1fcf6492b898a2ce19cbdb0
TranBin848 <tranbinn848@gmail.com>
2026-04-19 12:15:12 +0700

    Add check for building block gameobjects

--- Assets/Game/Scripts/Utils/PlacementProcess.cs
+++ Assets/Game/Scripts/Utils/PlacementProcess.cs
@@ -89,13 +89,33 @@ public class PlacementProcess
     bool CanPlaceTiles(Vector3Int tilePosition)
     {
 
-        return m_WalkableTilemap.HasTile(tilePosition) && !isInUnreachableTilemap(tilePosition);
+        return m_WalkableTilemap.HasTile(tilePosition) &&
+        !isInUnreachableTilemap(tilePosition) &&
+        !isBlockByGameObject(tilePosition);
     }
     bool isInUnreachableTilemap(Vector3Int tilePosition)
     {
         foreach (var tilemap in m_UnreachableTilemaps)
         {
             if (tilemap.HasTile(tilePosition))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    bool isBlockByGameObject(Vector3Int tilePosition)
+    {
+        Vector3 tileSize = m_WalkableTilemap.cellSize;
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(
+            new Vector2(tilePosition.x + tileSize.x / 2, tilePosition.y + tileSize.y / 2),
+            tileSize * 0.9f,
+            0f
+            );
+        foreach (var collider in colliders)
+        {
+            var layer = collider.gameObject.layer;
+            if (layer == LayerMask.NameToLayer("Unit") || layer == LayerMask.NameToLayer("Building"))
             {
                 return true;
             }
