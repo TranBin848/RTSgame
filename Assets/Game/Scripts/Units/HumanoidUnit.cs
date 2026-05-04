aab79b22772a6fdee8ef5d9d05b6706a4c85505c
TranBin848 <tranbinn848@gmail.com>
2026-05-04 21:53:21 +0700

    feat: update smoothing animation moving for unit

--- Assets/Game/Scripts/Units/HumanoidUnit.cs
+++ Assets/Game/Scripts/Units/HumanoidUnit.cs
@@ -3,6 +3,8 @@ public class HumanoidUnit : Unit
 {
     protected Vector2 m_Velocity;
     protected Vector3 m_lastPosition;
+    protected float m_SmoothFactor = 50;
+    protected float m_SmoothSpeed;
     public float CurrentSpeed => m_Velocity.magnitude;
     void Start()
     {
@@ -25,10 +27,12 @@ public class HumanoidUnit : Unit
             ) / Time.deltaTime;
 
         m_lastPosition = transform.position;
-        var state = m_Velocity.magnitude > 0.01f ? UnitState.Moving : UnitState.Idle;
+        m_SmoothSpeed = Mathf.Lerp(m_SmoothSpeed, CurrentSpeed, Time.deltaTime * m_SmoothFactor);
+
+        var state = m_SmoothSpeed > 0.1f ? UnitState.Moving : UnitState.Idle;
         SetState(state);
 
-        m_Animator.SetFloat("Speed", CurrentSpeed);
+        m_Animator.SetFloat("Speed", Mathf.Clamp01(m_SmoothSpeed));
     }
 }
 
